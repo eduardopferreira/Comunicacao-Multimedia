bcc3e4090245f48a1b595e860cc45ef0a772fccd
Eduardo Ferreira <eduardoferreira1329@gmail.com>
2024-05-06 15:10:37 +0100

    chore: Update HTML files and style.css for new recipe pages

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+const onGoing = "./images/onGoing/";
 const recipeDisplay = "./images/recipeDisplay/"; // Path to your images folder
 const bolo = "./images/bolo/";
 const onigiri = "./images/onigiri/";
 const panqueca = "./images/panqueca/";
 const guisado = "./images/guisado/";
 const imageExtension = "png"; // Extension of your image files
-const imageCount = 240; // Number of images you have
+const imageCount = 120; // Number of images you have
 const imageCount1 = 300; // Number of images you have
+const imageCount2 = 248; // Number of images you have
 
+const videoInicial = document.getElementById("onGoing");
 const videoToalha = document.getElementById("image");
 const videoBolo = document.getElementById("bolo");
 const videoOnigiri = document.getElementById("onigiri");
@@ -37,6 +40,36 @@ function preloadImages() {
     const img = new Image();
     img.src = `${guisado}${i.toString().padStart(4, "0")}.${imageExtension}`;
   }
+  for (let i = 1; i <= imageCount2; i++) {
+    const img = new Image();
+    img.src = `${onGoing}${i.toString().padStart(4, "0")}.${imageExtension}`;
+  }
+}
+
+function updateOnGoing() {
+  let currentImageIndex = 52;
+  let incrementing = true;
+
+  function changeImage() {
+    videoInicial.src = `${onGoing}${currentImageIndex
+      .toString()
+      .padStart(4, "0")}.${imageExtension}`;
+
+    if (incrementing) {
+      currentImageIndex++;
+      if (currentImageIndex === imageCount2) {
+        incrementing = false;
+      }
+    } else {
+      currentImageIndex--;
+      if (currentImageIndex === 52) {
+        incrementing = true;
+      }
+    }
+  }
+
+  // Set an interval to change images
+  setInterval(changeImage, 50); // Change image every 100 milliseconds
 }
 
 function updateBackground() {
@@ -74,7 +107,6 @@ function updateReceitas() {
     videoBolo.src = `${bolo}${currentImageIndex
       .toString()
       .padStart(4, "0")}.${imageExtension}`;
-
     if (incrementing) {
       currentImageIndex++;
       if (currentImageIndex === imageCount1) {
@@ -89,7 +121,6 @@ function updateReceitas() {
     videoOnigiri.src = `${onigiri}${currentImageIndex
       .toString()
       .padStart(4, "0")}.${imageExtension}`;
-
     if (incrementing) {
       currentImageIndex++;
       if (currentImageIndex === imageCount1) {
@@ -104,7 +135,6 @@ function updateReceitas() {
     videoPanqueca.src = `${panqueca}${currentImageIndex
       .toString()
       .padStart(4, "0")}.${imageExtension}`;
-
     if (incrementing) {
       currentImageIndex++;
       if (currentImageIndex === imageCount1) {
@@ -119,7 +149,6 @@ function updateReceitas() {
     videoGuisado.src = `${guisado}${currentImageIndex
       .toString()
       .padStart(4, "0")}.${imageExtension}`;
-
     if (incrementing) {
       currentImageIndex++;
       if (currentImageIndex === imageCount1) {
@@ -134,9 +163,10 @@ function updateReceitas() {
   }
 
   // Set an interval to change images
-  setInterval(changeImage, 50); // Change image every 100 milliseconds
+  setInterval(changeImage, 10); // Change image every 100 milliseconds
 }
 
 // Start updating the video with images
-updateBackground();
+updateOnGoing();
+//updateBackground();
 updateReceitas();
